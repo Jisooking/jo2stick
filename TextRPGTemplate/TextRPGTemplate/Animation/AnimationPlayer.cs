5d5471e4a9dd6fa01170ecfc6861d3f9f00d4f87
qwe1e34r56ty <sunwoonana@gmail.com>
2025-04-22 19:53:26 +0900

    init

---
+++ TextRPGTemplate/TextRPGTemplate/Animation/AnimationPlayer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextRPG.View;
+
+namespace TextRPGTemplate.Animation
+{
+    internal class AnimationPlayer
+    {
+        public AnimationPlayer()
+        {
+
+        }
+
+        public void play(Animation[] animations, SpriteView spriteView)
+        {
+            ViewPort viewPort = spriteView.view!;
+            int x = viewPort.x;
+            int y = viewPort.y;
+            
+
+            Thread thread = new(() =>
+            {
+                for (int i = 0; i < animations.Length; i++)
+                {
+                    for (int j = 0; j < animations[i].frames.Length; j++)
+                    {
+                        spriteView.SetText(animations[i].frames);
+                        spriteView.Update();
+                        spriteView.Render();
+                        Thread.Sleep(animations[i].frameDurationMs);
+                    }
+                }
+            });
+            thread.IsBackground = true;
+            thread.Start();
+            thread.Join();
+        }
+    }
+}
