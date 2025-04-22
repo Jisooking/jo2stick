74fbcbe7fbc57acf08ce53913d3cda8a1c8d4bf3
Jisooking <122238121+Jisooking@users.noreply.github.com>
2025-04-22 15:49:33 +0900

    base file
    base file

---
+++ TextRPGTemplate/Scene/ShopScene.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using TextRPG.Context;
+using TextRPG.View;
+
+namespace TextRPG.Scene
+{
+    internal class ShopScene : AScene
+    {
+        public ShopScene(GameContext gameContext, Dictionary<string, AView> viewMap, SceneText sceneText, SceneNext sceneNext) : base(gameContext, viewMap, sceneText, sceneNext)
+        {
+
+        }
+        public override void DrawScene()
+        {
+            ClearScene();
+
+            List<string> dynamicText = new();
+            dynamicText.Add("[보유 골드]");
+            dynamicText.Add($"{gameContext.ch.gold} G");
+            dynamicText.Add("");
+            dynamicText.Add("[아이템 목록]");
+            for (int i = 0; i < gameContext.shop?.items?.Count; i++)
+            {
+                Item tmp = gameContext.shop.items[i];
+                dynamicText.Add($"- {tmp.name} \t | {(tmp.attack > 0 ? "공격력" : "방어력")} + {(tmp.attack > 0 ? tmp.attack : tmp.guard)} \t | {(tmp.bought ? "구매완료" : tmp.price + "G")}");
+                dynamicText.Add($"\t {tmp.description}");
+            }
+            ((DynamicView)viewMap[ViewID.Dynamic]).SetText(dynamicText.ToArray());
+            ((SpriteView)viewMap[ViewID.Sprite]).SetText(sceneText.spriteText!);
+
+            Render();
+        }
+        public override string respond(int i)
+        {
+            return sceneNext.next![i];
+        }
+    }
+}
